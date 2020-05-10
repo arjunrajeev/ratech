a12ce9e984d426c50be1ff55279768c49416fbc4
Arjun Rajeev <arjun.rajeev@outlook.com>
2020-05-10 20:10:36 +0530

    Contact detail correction
    Updated the phone number to 510

--- src/app/common/services/constants.service.ts
+++ src/app/common/services/constants.service.ts
@@ -6,7 +6,7 @@ import { ServiceDetails } from '../models/service.detail.model';
 })
 export class ConstantsService {
   readonly companyContactEmail: string = 'info@ratechservices.in';
-  readonly companyContactLandPhone: string = '+91 85472 99410';
+  readonly companyContactLandPhone: string = '+91 85472 99510';
   readonly companyContactAddress: string = 'Ra Technical Services Pvt Ltd, 8/555 Kodumba, Palakkad, Kerala 678551';
   readonly companyContactLocation: string = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31359.179571362532!2d76.67573417108248!3d10.742386810047929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba86c56aaaaaaab%3A0xc7330273cd606d6a!2sRa%20Technical%20Services%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1589113244472!5m2!1sen!2sin';
 
